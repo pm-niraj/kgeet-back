ad740ea65deb07474e6bd36fd01ed6c568453293
NirajSangroula <sangroulaniraj15@gmail.com>
2025-02-09 17:51:57 -0500

    startup spring.io added

---
+++ src/main/java/com/pm_niraj/kgeet_back/KgeetBackApplication.java
@@ -0,0 +1,13 @@
+package com.pm_niraj.kgeet_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KgeetBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KgeetBackApplication.class, args);
+	}
+
+}
